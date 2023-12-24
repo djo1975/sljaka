85a47b7a9c7b2aa4a48c2f93efa461a7b4f07807
djo1975 <mladanilic1975@gmail.com>
2023-12-24 09:43:47 +0100

    Deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,16 +1,7 @@
-services:
-  - type: web
-    name: mysite
-    runtime: ruby
-    plan: free
-    buildCommand: "./bin/render-build.sh"
-    startCommand: "./bin/rails server -b 0.0.0.0"
-    envVars:
-      - key: DATABASE_URL
-        fromDatabase:
-          name: mysite
-          property: connectionString
-      - key: RAILS_MASTER_KEY
-        sync: false
-      - key: WEB_CONCURRENCY
-        value: 2
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+./bin/rails assets:precompile
+./bin/rails assets:clean
