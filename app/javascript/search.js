182e0845e7066a870d4c7ae09ace150a4e2ae2fd
djo1975 <mladanilic1975@gmail.com>
2023-12-27 17:40:27 +0100

    deploy

--- app/javascript/search.js
+++ app/javascript/search.js
@@ -7,26 +7,22 @@ document.addEventListener("DOMContentLoaded", function () {
       const query = searchInput.value.trim();
       
       if (query.length > 2) {
-        // Ako je upit duži od 2 karaktera, šaljemo AJAX zahtev
         sendSearchQuery(query);
       }
     });
   });
   
   function sendSearchQuery(query) {
-    // Šaljemo AJAX zahtev serveru
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "/searches/save_query", true);
     xhr.setRequestHeader("Content-Type", "application/json");
   
     xhr.onreadystatechange = function () {
       if (xhr.readyState === XMLHttpRequest.DONE) {
-        // Ovde možete dodati dodatnu logiku nakon završetka zahteva
         console.log("Server response:", xhr.responseText);
       }
     };
   
-    // Šaljemo upit kao JSON podatke
     xhr.send(JSON.stringify({ query: query }));
   }
   
